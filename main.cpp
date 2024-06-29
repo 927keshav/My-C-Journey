2c8e0f6795abcd203f878aedae60ceaff310b755
927keshav <146623223+927keshav@users.noreply.github.com>
2024-06-29 17:16:19 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,16 +1,32 @@
+/* Pseudocode 
+
+Prompt the user to enter the number of rooms
+
+Display number of rooms
+
+Display price per room
+
+Display cost: (number of rooms*price per room)
+
+Display tax: number of rooms*price per room*tax rate
+
+Display total estimate: (number of rooms*price per room)+(number of rooms*price per room*tax rate)
+Display estimate expiration time*/
+
+
 #include<iostream>
 
 using namespace std;
 int main() {
 	int number_of_rooms {0};
-	cout<<"Welcome to the cleaning services"<<endl;
+	cout<<"Welcome to the Keshav's carpet cleaning services"<<endl;
 	cout<<"\nHow many rooms would you like to be cleaned"<<endl;
 	cin>>number_of_rooms;
 	const double price_per_room {30.0};
 	const double sales_tax {0.06};
 	const int estimate_expiry {30};
 	
-	cout<<"Estimate for carpet cleaning service"<<endl;
+	cout<<"Estimate for  Keshav's carpet cleaning service"<<endl;
 	cout<<"Number of rooms:"<<number_of_rooms<<endl;
 	cout<<"Price per room:"<<price_per_room <<endl;
 	cout<<"Cost:$"<<price_per_room *number_of_rooms<<endl;
@@ -21,3 +37,14 @@ int main() {
 	return 0;
 	
 }
+/* Here the  Welcome to the cleaning services
+
+How many rooms would you like to be cleaned
+2
+Estimate for carpet cleaning service
+Number of rooms:2
+Price per room:30
+Cost:$60
+Tax:$0.06
+============================================
+Total estimate   63.6
